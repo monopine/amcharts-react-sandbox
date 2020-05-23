84bf86cd782247be05507755c883860a54c67a79
brian <brian@monopine.com>
2020-05-23 12:54:48 -0700

    run build

---
+++ build/precache-manifest.45cc9b1cb7d0ebef43bdc0d44b4862ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6659eeea5e803e2d10bebcf1897c62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a515bae1dcc9a859fdc",
+    "url": "/static/css/main.fe6478c5.chunk.css"
+  },
+  {
+    "revision": "1c0e4716cb2ba8d0d5a6",
+    "url": "/static/js/3.e2907b40.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/static/js/3.e2907b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63f19d61920a7a8214d",
+    "url": "/static/js/4.ec31006a.chunk.js"
+  },
+  {
+    "revision": "d45e714be14cdcd3392b7d49a758c965",
+    "url": "/static/js/4.ec31006a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd82e0b420f71b53420",
+    "url": "/static/js/5.a14e0601.chunk.js"
+  },
+  {
+    "revision": "09fd7d4d35d97826275faa79146aea9f",
+    "url": "/static/js/5.a14e0601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09c2c4887ed7e2c02963",
+    "url": "/static/js/6.9d5123a9.chunk.js"
+  },
+  {
+    "revision": "1a515bae1dcc9a859fdc",
+    "url": "/static/js/main.f5c04ece.chunk.js"
+  },
+  {
+    "revision": "867cf424b0b25548b610",
+    "url": "/static/js/runtime-main.e084ad88.js"
+  },
+  {
+    "revision": "d2467b65be8b59b9cb28",
+    "url": "/static/js/xlsx.b0db2d1a.chunk.js"
+  }
+]);
---
+++ build/static/js/xlsx.b0db2d1a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpamcharts-demo"]=this["webpackJsonpamcharts-demo"]||[]).push([[2],{145:function(n,c){},156:function(n,c){},157:function(n,c){}}]);
+//# sourceMappingURL=xlsx.b0db2d1a.chunk.js.map
